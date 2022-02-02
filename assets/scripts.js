a0b4008954e0474edec21d5430e4afb733834e9b
BITCHARD <richgarcia7525@gmail.com>
2022-02-01 21:35:10 -0600

    added the questions for the user to choose from along with the answers.

---
+++ assets/scripts.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        question: "Why do JavaScript and Java have similiar names?",
+        a: "JavaScript is a stripped-down version of Java",
+        b: "JavaScript's syntax is loosley based on Java's",
+        c: "They both originated on the island of Java",
+        d: "None of the above",
+        correct: "b",
+    },
+    {
+        question: "____JavaScript is also called server-side JavaScript.",
+        a: "Microsoft",
+        b: "Navigator",
+        c: "LiveWire",
+        d: "Native",
+        correct: "c",
+    },
+    {
+        question: "Inside which HTML element do we put JavaScript?",
+        a: "<js>",
+        b: "<scripting>",
+        c: "<script>",
+        d: "<javascript>",
+        correct: "c",
+    },
+    {
+        question: "Choose the server-side JavaScript object?",
+        a: "FileUpLoad",
+        b: "Function",
+        c: "File",
+        d: "Date",
+        correct: "c",
+    },
+    {
+        question: "Choose the client-side JavaScript object?",
+        a: "Database",
+        b: "Cursor",
+        c: "Client",
+        d: "FileUpLoad",
+        correct: "d",
+    },
+];
